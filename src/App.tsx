ba43d624a8aefab5889d03dda3daa53b0fd96c2f
Jay Nakrani <redacted@redacted.com>
2021-12-01 10:39:45 -0500

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+          Have some jalebi fafda. They are yummy! 😋
+      </header>
+    </div>
+  );
+}
+
+export default App;
